c25962e5000da33813c69542bc3429a00210c728
prototype2010 <Firstday2010>
2021-05-17 15:19:19 +0300

    active record works fine

--- db/schema.rb
+++ db/schema.rb
@@ -1,6 +1,20 @@
-ActiveRecord::Schema.define(version: 20_170_126_191_401) do
-  create_table 'notes', force: :cascade do |t|
-    t.integer 'note'
-    t.integer 'amount', default: 0, null: false
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2017_01_26_191401) do
+
+  create_table "notes", force: :cascade do |t|
+    t.integer "note"
+    t.integer "amount", default: 0, null: false
   end
+
 end
